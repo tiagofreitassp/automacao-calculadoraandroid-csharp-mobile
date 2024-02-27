f89523923c78882cffca49064ebf843b4495ab67
Tiago Freitas <tiago.freitas.sp@gmail.com>
2024-02-27 20:29:09 -0300

    Atualizado gitignore

---
+++ automacao-calculadoraandroid-csharp-mobile/automacao-calculadoraandroid-csharp-mobile/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace automacao_calculadoraandroid_csharp_mobile;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
